88cb23147311ac42572cc11b52962526692d49a5
Teemu Talja <teemu.talja@helsinki.fi>
2016-04-19 15:10:20 +0300

    Image gallery hover was replaced with very subtle typography.

--- themes/custom/js/custom.js
+++ themes/custom/js/custom.js
@@ -1,6 +1,11 @@
 /* Add your custom template javascript here */
 
 function finnaCustomInit() {
-
-
+    var allParas = document.getElementsByClassName('dateyeartype');
+    //filter by class name if desired...
+    for(var i=0;i<allParas.length;i++) {
+        if(allParas[i].getElementsByTagName('*').length == 0){
+            allParas[i].style.display = 'none';
+        }
+    }
 }
